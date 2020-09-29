c63b8c014c5da3c4a92ca6e8436b15fde04b91d4
TatiynaLN <68844852+TatiynaLN@users.noreply.github.com>
2020-09-30 00:07:45 +0300

    Верстка body+footer

---
+++ js/main.js
@@ -0,0 +1,10 @@
+$(document).ready(function(){
+    $('.photo__slider').slick({
+        arrows:true,
+        adaptiveHeight:true,
+        slidesToShow:1,
+        slidesToScroll:1,
+        speed:1000,
+        
+    });
+  });
